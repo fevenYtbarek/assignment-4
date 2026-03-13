ded5db3538817bbe4823b20c62ea1cf2bf8db6b4
Paolo Cruz <paolo.cruz24@gmail.com>
2026-03-12 23:57:49 -0600

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,30 +1,32 @@
 import { useRouter } from "expo-router";
 import React from "react";
-import { Pressable, StyleSheet, Text } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   const router = useRouter();
   return (
     <SafeAreaView style={styles.container}>
-      <Pressable
-        style={styles.employeeBtn}
-        onPress={() => router.push("/employee-form")}
-      >
-        <Text style={styles.btnText}>Employee Form</Text>
-      </Pressable>
-      <Pressable
-        style={styles.employeeBtn}
-        onPress={() => router.push("/sign-up")}
-      >
-        <Text style={styles.btnText}>Sign Up</Text>
-      </Pressable>
-      <Pressable
-        style={styles.employeeBtn}
-        onPress={() => router.push("/log-in")}
-      >
-        <Text style={styles.btnText}>Log In</Text>
-      </Pressable>
+      <View style={styles.view}>
+        <Pressable
+          style={styles.employeeBtn}
+          onPress={() => router.push("/employee-form")}
+        >
+          <Text style={styles.btnText}>Employee Form</Text>
+        </Pressable>
+        <Pressable
+          style={styles.employeeBtn}
+          onPress={() => router.push("/sign-up")}
+        >
+          <Text style={styles.btnText}>Sign Up</Text>
+        </Pressable>
+        <Pressable
+          style={styles.employeeBtn}
+          onPress={() => router.push("/log-in")}
+        >
+          <Text style={styles.btnText}>Log In</Text>
+        </Pressable>
+      </View>
     </SafeAreaView>
   );
 }
@@ -32,10 +34,16 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 24,
-    justifyContent: "space-between",
-    marginVertical: 400,
+    backgroundColor: "black",
   },
+
+  view: {
+    flex: 1,
+    padding: 24,
+    justifyContent: "center",
+    gap: 25,
+  },
+
   employeeBtn: {
     backgroundColor: "#4F46E5",
     padding: 16,
